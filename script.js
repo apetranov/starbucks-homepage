5b04e098dd1fd72031e1e5f490e07be0bda83513
Alexander Petranov <shasho68.5@gmail.com>
2024-05-31 20:36:44 +0300

    Add some mobile responsiveness

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var downArrow1 = document.getElementById("downarrow1");
+    downArrow1.addEventListener("click", function() {
+        console.log("Clicked!");
+    })
+});
